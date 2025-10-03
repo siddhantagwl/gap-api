0a1db34fbc96232233190c69e97cefda23a5e442
Siddhant Agarwal <siddhantagwl29@gmail.com>
2025-10-03 14:30:51 +0200

    add POST /service-requests endpoint

--- src/service-requests/service-requests.controller.ts
+++ src/service-requests/service-requests.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { ServiceRequestsService } from './service-requests.service';
+import { CreateServiceRequestDto } from './dto/create-service-request.dto';
 
-@Controller('service-requests')
-export class ServiceRequestsController {}
+@Controller('service-requests') // listen to /service-requests route
+export class ServiceRequestsController {
+  constructor(private readonly service: ServiceRequestsService) {}
+
+  @Post()
+  create(@Body() dto: CreateServiceRequestDto) {
+    // grabs the JSON data from the request body
+    // and passes it to the service layer to handle the business logic
+    return this.service.create(dto);
+  }
+}
