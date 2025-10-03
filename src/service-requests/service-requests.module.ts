3d57aec036a628e46a06b2b2ce23e27a33ed50b4
Siddhant Agarwal <siddhantagwl29@gmail.com>
2025-10-03 14:32:58 +0200

    refactor: reorganize imports and structure of ServiceRequestsModule

--- src/service-requests/service-requests.module.ts
+++ src/service-requests/service-requests.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
-import { ServiceRequestsService } from './service-requests.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServiceRequestsController } from './service-requests.controller';
+import { ServiceRequestsService } from './service-requests.service';
+import { ServiceRequest, ServiceRequestSchema } from './schemas/service-request.schema';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: ServiceRequest.name, schema: ServiceRequestSchema }]),
+    UsersModule,
+  ],
+  controllers: [ServiceRequestsController],
   providers: [ServiceRequestsService],
-  controllers: [ServiceRequestsController]
 })
 export class ServiceRequestsModule {}
